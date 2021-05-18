5cba9fbb12f0ad052147d90a321f5c71362f595f
jurgecn <njurgec@hotmail.com>
2021-05-18 13:49:32 +0200

    1

--- YurgiOne/Startup.cs
+++ YurgiOne/Startup.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using YurgiOne.Services;
 
 namespace YurgiOne
 {
+
     public class Startup
     {
+       
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,6 +28,7 @@ namespace YurgiOne
                 app.UseDeveloperExceptionPage();
             else
                 app.UseExceptionHandler("/error");
+            app.UseHsts();
 
             app.UseStaticFiles();
             app.UseNodeModules();
